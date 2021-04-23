0aa65d7f4b0cee8445571dd3bb4b2c8f2888b5eb
Karol Krol <Karol.Krol@vimn.com>
2021-04-24 01:11:30 +0200

    Antoher change...

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const favoriteFood = process.env.FAVORITE_FOOD;
+const FAVORITE_FOOD = process.env.NAME;
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -7,7 +7,7 @@ function sleep(ms) {
 async function main() {
   while(true) {
     console.log('Containers rule!');
-    console.log(`My favorite food is ${favoriteFood}`);
+    console.log(`My favorite food is ${FAVORITE_FOOD}`);
     await sleep(5000);
   }
 }
